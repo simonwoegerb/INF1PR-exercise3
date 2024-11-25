8185b63346a47ac35c63dbb094aad9dfadf4bab0
Simon Wögerbauer <simonwoegerbauer@outlook.com>
2024-11-25 10:06:07 +0100

    added algorithm files

---
+++ subtask1.py
@@ -0,0 +1,7 @@
+def largestSquareNumber(n: int):
+    q = 1
+    while n>(q*q):
+        q = q+1
+    q = (q-1)*(q-1)
+    return q
+print(largestSquareNumber(82))
---
+++ subtask2.py
@@ -0,0 +1,21 @@
+def sequence():
+    n = 0
+    m = 0
+    s = 0
+    a = 0
+    x = int(input())
+    m = x # first is always smallest
+    while x != -1:
+        n = n+1
+        s = s+x
+        if m>x:
+            print(x)
+            m = x
+        x = int(input())
+    if n != 0:
+        a = s/n
+    else:
+        m = -1
+        a = -1
+    return (n,s,m,a)
+print(sequence())
